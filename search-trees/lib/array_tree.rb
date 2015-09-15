275ffd31bcc4c45db2cc03de1d62edc88a28f5a5
Paul Mucur <mudge@mudge.name>
2015-09-15 21:04:09 +0100

    Implement ArrayTree#search

---
+++ search-trees/lib/array_tree.rb
@@ -0,0 +1,45 @@
+class ArrayTree
+  attr_reader :top
+  private :top
+
+  def initialize(items, left, right, top = 1)
+    @items = items
+    @left = left
+    @right = right
+    @top = top
+  end
+
+  def search(name)
+    found = false
+    node = top
+
+    until node.nil?
+      if items(node) == name
+        puts "Yes"
+        node = nil
+        found = true
+      elsif items(node) > name
+        node = left(node)
+      elsif items(node) < name
+        node = right(node)
+      end
+    end
+
+    puts "No" unless found
+  end
+
+  private
+
+  def items(node)
+    @items[node.pred]
+  end
+
+  def left(node)
+    @left[node.pred]
+  end
+
+  def right(node)
+    @right[node.pred]
+  end
+end
+
---
+++ search-trees/spec/array_tree_spec.rb
@@ -0,0 +1,27 @@
+require 'array_tree'
+
+RSpec.describe ArrayTree do
+  describe '#search' do
+    it 'prints "Yes" if it finds the name' do
+      tree = described_class.new(
+        [nil, nil, 37, 29,  51, nil, 55, 53,  62,  49,  5,   nil, 17,  nil],
+        [nil, nil, 4,  11,  10, nil, 8,  nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, 5,  nil, 7,  nil, 9,  nil, nil, nil, 13,  nil, nil, nil],
+        3
+      )
+
+      expect { tree.search(17) }.to output("Yes\n").to_stdout
+    end
+
+    it 'prints "No" if it does not find the name' do
+      tree = described_class.new(
+        [nil, nil, 37, 29,  51, nil, 55, 53,  62,  49,  5,   nil, 17,  nil],
+        [nil, nil, 4,  11,  10, nil, 8,  nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, 5,  nil, 7,  nil, 9,  nil, nil, nil, 13,  nil, nil, nil],
+        3
+      )
+
+      expect { tree.search(999) }.to output("No\n").to_stdout
+    end
+  end
+end
