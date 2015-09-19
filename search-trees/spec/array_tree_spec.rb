2e0639cda9c133e2b9bd0cff06d144eae4875164
Paul Mucur <mudge@mudge.name>
2015-09-19 17:57:43 +0100

    Extract tree building in the tests

--- search-trees/spec/array_tree_spec.rb
+++ search-trees/spec/array_tree_spec.rb
@@ -3,23 +3,13 @@ require 'array_tree'
 RSpec.describe ArrayTree do
   describe '#search' do
     it 'prints "Yes" if it finds the name' do
-      tree = described_class.new(
-        [nil, nil, 37, 29,  51, nil, 55, 53,  62,  49,  5,   nil, 17,  nil],
-        [nil, nil, 3,  10,  9,  nil, 7,  nil, nil, nil, nil, nil, nil, nil],
-        [nil, nil, 4,  nil, 6,  nil, 8,  nil, nil, nil, 12,  nil, nil, nil],
-        2
-      )
+      tree = build_tree
 
       expect { tree.search(17) }.to output("Yes\n").to_stdout
     end
 
     it 'prints "No" if it does not find the name' do
-      tree = described_class.new(
-        [nil, nil, 37, 29,  51, nil, 55, 53,  62,  49,  5,   nil, 17,  nil],
-        [nil, nil, 3,  10,  9,  nil, 7,  nil, nil, nil, nil, nil, nil, nil],
-        [nil, nil, 4,  nil, 6,  nil, 8,  nil, nil, nil, 12,  nil, nil, nil],
-        2
-      )
+      tree = build_tree
 
       expect { tree.search(999) }.to output("No\n").to_stdout
     end
@@ -27,12 +17,7 @@ RSpec.describe ArrayTree do
 
   describe '#depth' do
     it 'prints the tree in depth-first order' do
-      tree = described_class.new(
-        [nil, nil, 37, 29,  51, nil, 55, 53,  62,  49,  5,   nil, 17,  nil],
-        [nil, nil, 3,  10,  9,  nil, 7,  nil, nil, nil, nil, nil, nil, nil],
-        [nil, nil, 4,  nil, 6,  nil, 8,  nil, nil, nil, 12,  nil, nil, nil],
-        2
-      )
+      tree = build_tree
 
       expect { tree.depth }.to output("37\n29\n5\n17\n51\n49\n55\n53\n62\n").to_stdout
     end
@@ -40,14 +25,19 @@ RSpec.describe ArrayTree do
 
   describe '#breadth' do
     it 'prints the tree in breadth-first order' do
-      tree = described_class.new(
-        [nil, nil, 37, 29,  51, nil, 55, 53,  62,  49,  5,   nil, 17,  nil],
-        [nil, nil, 3,  10,  9,  nil, 7,  nil, nil, nil, nil, nil, nil, nil],
-        [nil, nil, 4,  nil, 6,  nil, 8,  nil, nil, nil, 12,  nil, nil, nil],
-        2
-      )
+      tree = build_tree
 
       expect { tree.breadth }.to output("37\n29\n51\n5\n49\n55\n17\n53\n62\n").to_stdout
     end
   end
+
+  def build_tree
+    described_class.new(
+      [nil, nil, 37, 29,  51, nil, 55, 53,  62,  49,  5,   nil, 17,  nil],
+      [nil, nil, 3,  10,  9,  nil, 7,  nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, 4,  nil, 6,  nil, 8,  nil, nil, nil, 12,  nil, nil, nil],
+      2
+    )
+  end
 end
+
